117a6c8fcaa8745756c28d58d59c3a15a176a749
DevAS <jeremy.w.marin@gmail.com>
2023-11-17 23:47:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,6 @@
 const nbJoursDansLeMois = () => {
-	let nombreJour = 0;
-	let date = new Date();
-	if(date.getMonth() <= 6){
-		if(date.getMonth() % 2 == 0){
-			nombreJour = 31;
-		}else{
-			nombreJour = 30;
-		}
-	}else{
-		if(date.getMonth() % 2 == 1){
-			nombreJour = 30;
-		}else{
-			nombreJour = 31;
-		}
-	}
-	if(date.getMonth() == 1){
-		if(date.getFullYear() % 4 == 0){
-			if(date.getFullYear() % 100 == 0){
-				if(date.getFullYear() % 400 == 0){
-					nombreJour = 29;
-				}else{
-					nombreJour = 28;
-				}
-			}else{
-				nombreJour = 29;
-			}
-		}else{
-			nombreJour = 28;
-		}
-	}
-	return nombreJour;
-}
+    const dateActuelle = new Date();
+    return new Date(dateActuelle.getFullYear(), dateActuelle.getMonth() + 1, 0).getDate();
+};
+
 module.exports = nbJoursDansLeMois;
